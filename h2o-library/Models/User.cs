0a3f003cd98fdecd0f8c5b449c55336bbcd964e8
yannick <Y.achterstraat@gmail.com>
2025-12-24 13:31:11 +0100

    Hotfix: nuked broken projects

--- h2o-library/Models/User.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace h2o_library.Models;
-
-public class User
-{
-    
-}
--- h2o-library/Repositories/UserRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using h2o_library.Abstract.Repositories;
-
-namespace h2o_library.Repositories;
-
-public class UserRepository: IUserRepository
-{
-    
-}
